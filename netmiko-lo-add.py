bec8e84c12d3c0b6143b2724d8ecfae6c2f550b8
sudop0wer <tugleacristian@gmail.com>
2024-10-29 21:44:50 +0200

    finished

--- netmiko-lo-add.py
+++ netmiko-lo-add.py
@@ -6,18 +6,19 @@ from netmiko import ConnectHandler
 
 net_connect = ConnectHandler(
     device_type="cisco_xe",
-    host="192.168.1.134",
+    host="192.168.1.135",
     username="cisco",
     password="cisco123!",
 )
 
 command_list = [
-    "interface lo 10",
-    "ip add 10.10.10.1 255.255.255.0",
+    "interface lo 30",
+    "ip add 10.10.30.3 255.255.255.255",
     "description created_with_netmiko",
     "no shut"
+    "do write"
 ]
 
 net_connect.send_config_set(command_list)
 print(net_connect.send_command("show ip int br"))
-print(net_connect.send_command("show interface lo 10"))
+print(net_connect.send_command("show interface lo 20"))
